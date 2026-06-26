e4b8d45f80b1a652711997e1061d1369bbe128b8
ErickFSilvaProg <erick.ferreira.prog@gmail.com>
2026-06-26 11:57:39 -0300

    Curso_Python3_LuizOtavio, 26/06/26 (manhã)

---
+++ Secao_03_iniciando_na_prog_python/aula017_interpretador_comentarios.py
@@ -0,0 +1,4 @@
+# Comentário de linha
+
+# Imprime no terminal: Python 3
+print('Python', 1 + 2)
---
+++ Secao_03_iniciando_na_prog_python/aula018_docstrings_como_comentarios.py
@@ -0,0 +1,8 @@
+# Isso é um comentário no Python.
+
+"""
+    Docstrings...
+    Isso não é um comentário no Python.
+    O interpretador do Python ler esse código.
+    Mas pode ser utilizado como comentário de código.
+"""
