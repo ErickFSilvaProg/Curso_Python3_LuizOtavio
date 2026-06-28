a0fdb19acf6902b02e6d348054848efe8a8881d9
ErickFSilvaProg <erick.ferreira.prog@gmail.com>
2026-06-27 21:12:15 -0300

    Curso_Python3_LuizOtavio, 27/06/26 (noite)

---
+++ Secao_03_iniciando_na_prog_python/aula019_funcao_print.py
@@ -0,0 +1,24 @@
+"""
+    — Função print().
+
+        Usada para exibição em tela.
+        
+        Essa função recebe argumentos.
+            Ex: print('argumento')
+        
+        Por padrão, a função 'print()' adiciona um espaço após a exibição de cada argumento e uma quebra de linha.
+
+        O Python diferencia letras maiúsculas de letras minúsculas.
+"""
+
+# Argumentos não nomeados:
+print(12,34)
+print(56,78)
+print()
+
+
+# Argumentos nomeados:
+print(27,'06',2026, sep='/')
+print('Sequência de Fibonacc:',sep=':',end=" ")
+print(1,1,2,3,5,8,13,21,34,sep=' • ',end=' ...')
+print()
