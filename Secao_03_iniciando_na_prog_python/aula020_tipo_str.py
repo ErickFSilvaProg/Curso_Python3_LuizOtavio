c74d62a150c98df1993b82487abea6bbeeaf483f
ErickFSilvaProg <erick.ferreira.prog@gmail.com>
2026-07-03 11:18:37 -0300

    Curso_Python3_LuizOtavio, 03/07/26 (manhã)

---
+++ Secao_03_iniciando_na_prog_python/aula020_tipo_str.py
@@ -0,0 +1,30 @@
+"""
+    Tipo primitivo string:
+
+        str → string → texto
+        Strings são textos que estão dentro de aspas, simples ou duplas.
+
+    Tipagem do Python: Dinâmica e forte
+
+"""
+
+
+# Exemplo: Inteiro
+print('String →',1234)
+print()
+
+
+# Aspas simples: String
+print('Erick Ferreira')
+
+
+# Aspas duplas: String
+print('Erick Ferreira, "programador python".')
+
+
+# Escape:
+print('Erick Ferreira, \'programador Python\'.')
+
+
+# r (expressões regulares):
+print(r"Erick Ferreira, \"programador Python\".")
---
+++ Secao_03_iniciando_na_prog_python/aula021_tipo_int_e_float.py
@@ -0,0 +1,47 @@
+"""
+    Tipos primitivos int e float
+
+    O Python distingue os números com e sem casas decimais.
+    
+
+    — int: número inteiro
+
+        O tipo int representa qualquer número positivo ou negativo.
+        int sem sinal é considerado positivo.
+    
+    — float: número com ponto flutuante
+
+        O tipo float representa qualquer número positivo ou negativo com ponto flutuante.
+        float sem sinal é considerado positivo.
+    
+    A função "type" mostra o tipo que o Python inferiu ao valor.
+    Na programação o ponto "." sempre será utilizado para separar as casas decimais de um número.
+    Tudo em Python é um objeto.
+
+"""
+
+
+# Números inteiros:
+print(11)
+print(-11)
+print(0)
+print()
+
+
+# Números float:
+print(1.1, 10.11)
+print(-1.1)
+print()
+
+
+# Função "type": Identifica o tipo do valor.
+print(type(10), 10)
+print(type(0), 0)
+print()
+
+print(type(11.10), 11.10)
+print(type(-1.1), -1.1)
+print()
+
+print(type('Python'), 'Python')
+print()
