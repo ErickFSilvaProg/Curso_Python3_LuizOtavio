2fc909099bf3db3665dfbcaa269216141763388d
ErickFSilvaProg <erick.ferreira.prog@gmail.com>
2026-09-03 16:04:45 -0300

    Curso_Python3_LuizOtavio, 03/09/26 (tarde)

--- Secao_03_iniciando_na_programacao_python/aula048_exercicio_teste_seu_conhecimento.py
+++ Secao_03_iniciando_na_programacao_python/aula048_exercicio_teste_seu_conhecimento.py
@@ -8,29 +8,48 @@
 
                 a. Seu nome é {nome}
                 b. Seu nome invertido é {nome_invertido}
-                c. Se o nome contém (ou não) espaços
+                c. Seu nome contém (ou não) espaços
                 d. Seu nome tem {n} letras
                 e. A primeira letra do seu nome é {letra}
                 f. A última letra do seu nome é {letra}
         
-        4. Se nada for digitado em nome ou idade:
+        4. Se nada for digitado em nome e em idade:
             ↪ Exiba:
 
                 a. "Desculpe, você deixou campos vazios"
 
 """
 
-# PROGRAMA...
+# PROGRAM...
 
-# Variáveis:
-nome = input('Qual seu nome? ')
-idade = input('Qual sua idade? ')
+# Variables:
+name = input('What\'s your name? ')
+age = input('What\'s your age? ')
 print()
 
-if nome and idade:
-    print(f'Seu nome é {nome}')
-    print(f'')
-    print(f'')
-    print(f'')
-    print(f'')
-    print(f'')
+
+if name and age:
+
+    name_reversed = name[::-1]
+    name_space = ...
+    numbers_of_letter = len(name)
+    first_letter = name[0]
+    last_letter = name[-1]
+
+
+    if ' ' in name:
+        name_space = 'contém'
+    else:
+        name_space = 'não comtém'
+
+
+    print(f'Your name is {name}')
+    print(f'Your name reversed is {name_reversed}')
+    print(f'Your name contains (or does not contains) {name_space} space(s)')
+    print(f'Your name has {numbers_of_letter} letter(s)')
+    print(f'The first letter of your name is {first_letter}')
+    print(f'The last letter of your name is {last_letter}')
+else:
+    print('Sorry, you left fields blank!')
+
+print()
